fea94e52b5b13d01708d7db7b80fb11f4198b774
TheNQ <TheNQ>
2025-05-21 15:30:53 +0700

    2. Create file Prettier and Config file alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@components': '/src/components',
+      '@assets': '/src/assets',
+      '@hooks': '/src/hooks',
+      '@utils': '/src/utils',
+      '@context': '/src/context',
+      '@pages': '/src/pages',
+      '@styles': '/src/styles',
+      '@routes': '/src/routes',
+      '@constants': '/src/constants',
+      '@types': '/src/types',
+      '@services': '/src/services',
+      '@store': '/src/store',
+      '@api': '/src/api',
+      '@config': '/src/config',
+      '@mocks': '/src/mocks',
+      '@tests': '/src/tests',
+      '@locales': '/src/locales',
+      '@lib': '/src/lib',
+      '@middleware': '/src/middleware',
+      '@helpers': '/src/helpers',
+      '@images': '/src/assets/images',
+      '@fonts': '/src/assets/fonts',
+      '@videos': '/src/assets/videos',
+      '@audios': '/src/assets/audios',
+      '@icons': '/src/assets/icons',
+      '@svg': '/src/assets/svg',
+    },
+  },
+});
