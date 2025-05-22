f1509c292e1891ea254c99a62433bf37c7383c77
TheNQ <TheNQ>
2025-05-22 10:01:30 +0700

    3. Config vite alias. Create structure

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,6 @@
+
+function MainLayout({children}: {children: React.ReactNode}) {
+  return ( <main> {children} </main> );
+}
+
+export default MainLayout;
--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,15 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-      '@components': '/src/components',
-      '@assets': '/src/assets',
-      '@hooks': '/src/hooks',
-      '@utils': '/src/utils',
-      '@context': '/src/context',
-      '@pages': '/src/pages',
-      '@styles': '/src/styles',
-      '@routes': '/src/routes',
-      '@constants': '/src/constants',
-      '@types': '/src/types',
-      '@services': '/src/services',
-      '@store': '/src/store',
-      '@api': '/src/api',
-      '@config': '/src/config',
-      '@mocks': '/src/mocks',
-      '@tests': '/src/tests',
-      '@locales': '/src/locales',
-      '@lib': '/src/lib',
-      '@middleware': '/src/middleware',
-      '@helpers': '/src/helpers',
-      '@images': '/src/assets/images',
-      '@fonts': '/src/assets/fonts',
-      '@videos': '/src/assets/videos',
-      '@audios': '/src/assets/audios',
-      '@icons': '/src/assets/icons',
-      '@svg': '/src/assets/svg',
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
   },
 });
